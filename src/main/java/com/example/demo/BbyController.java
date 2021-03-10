4814561fa7bb7f63a0c38e3d38b1765affddadc3
tintun2602 <tintunn26@gmail.com>
2021-03-10 22:45:43 +0100

    Finito burrito

---
+++ src/main/java/com/example/demo/BbyController.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BbyController {
+    private final List<Biletter> filmListe = new ArrayList<>();
+
+    @PostMapping("/lagre")
+    public void visResultat(Biletter inputFilm) {
+        filmListe.add(inputFilm);
+
+    }
+
+    @GetMapping("/biletter")
+    public List<Biletter> hentAlle() {
+        return filmListe;
+    }
+
+    @GetMapping("/slett")
+    public void slettBilett() {
+        filmListe.clear();
+    }
+
+}
